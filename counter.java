ec9227facf23f754b293c5086d0d0d554f29f83b
unknown <shashank24mahendrakar@gmail.com>
2023-07-11 23:25:40 +0530

    first project

--- counter.java
+++ counter.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class counter {
     public static void main(String args[]) {
-        System.out.println("heyy shash");
+        System.out.println("I have startted something new");
 
     }
 }

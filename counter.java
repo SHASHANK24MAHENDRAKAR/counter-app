36df0962989cfeea7c048d0cbf0e96f8113b7c3a
unknown <shashank24mahendrakar@gmail.com>
2023-08-22 23:05:35 +0530

    changed

--- counter.java
+++ counter.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class counter {
     public static void main(String args[]) {
-        System.out.println("I have startted something new");
+        System.out.println("I have started something new");
 
     }
 }
